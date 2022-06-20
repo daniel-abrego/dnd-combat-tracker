3306c3dadd575f2629483b8edba3a605d71060b4
Daniel Abrego <danielabrego@utexas.edu>
2022-06-20 12:27:45 -0500

    updates

--- dnd/combatant.py
+++ dnd/combatant.py
@@ -1,45 +1,48 @@
+from enum import Enum
 from random import randint
 
 class Combatant:
+    class Stats(Enum):
+        CON = 0
+        DEX = 1
+        STR = 2
+        INT = 3
+        WIS = 4
+        CHA = 5
+    
     name = ''
     stats = {}
     initiative = 0
-    initiative_mod = 0
     
-    def __init__(self, name):
+    def __init__(self, name, stats):
         self.name = name
-        self.stats = {
-            'con': self.generate_stat(),
-            'str': self.generate_stat(),
-            'dex': self.generate_stat(),
-            'int': self.generate_stat(),
-            'wis': self.generate_stat(),
-            'cha': self.generate_stat()
+        if stats:
+            self.stats = stats
+        else:
+            self.stats = {
+                self.Stats.CON: self.generate_stat(),
+                self.Stats.STR: self.generate_stat(),
+                self.Stats.DEX: self.generate_stat(),
+                self.Stats.INT: self.generate_stat(),
+                self.Stats.WIS: self.generate_stat(),
+                self.Stats.CHA: self.generate_stat()
         }
         self.initiative = self.get_initiative()
-        self.initiative_mod = (self.stats['dex'] - 10) // 2
 
     def get_initiative(self):
         return self.initiative
+
+    def get_initiative_mod(self):
+        return (self.get_initiative() - 10) // 2
     
     def roll_initiative(self):
-        self.initiative = randint(1,20) + self.initiative_mod
+        self.initiative = randint(1,20) + self.get_initiative_mod()
         return self.initiative
 
-    def change_stats(self, con, str, dex, int, wis, cha):
-        def validate_stat(stat):
-            return stat <= 20 and stat >= 1
+    def change_stats(self, stat, new_stat_val):
+        
+        assert(new_stat_val <= 20 and new_stat_val >= 1)
 
-        if con and validate_stat(con):
-            self.stats['con'] = con
-        if str and validate_stat(str):
-            self.stats['str'] = str
-        if dex and validate_stat(dex):
-            self.stats['dex'] = dex
-        if int and validate_stat(int):
-            self.stats['int'] = int
-        if wis and validate_stat(wis):
-            self.stats['wis'] = wis
 
     def validate_stat(self, stat):
         return stat <= 20 and stat >= 1
@@ -55,6 +58,12 @@ class Combatant:
         dice_rolls.remove(lowest_roll)
         assert(True)
         return sum(dice_rolls)
+    
+    def __repr__(self):
+        return "Combatant()"
+
+    def __str__(self):
+        return str(self.name) + '(' + str(self.get_initiative_mod()) + ')'
     
 if __name__=='__main__':
     test = Combatant(name='test')
--- dnd/initiative.py
+++ dnd/initiative.py
@@ -2,28 +2,40 @@ import combatant
 
 class Initiative:
     def __init__(self):
-        self.initiative = []
+        self.initiative = {}
         self.combatants = []
         self.rounds = 0
 
     def add_combatant(self, new_combatant: combatant, initiative: int, dex_mod: int):
+        if new_combatant not in self.combatants:
+            self.combatants.append(new_combatant)
+        
         if initiative not in self.initiative:
-            self.inititiave[initiative] = []
+            self.initiative[initiative] = []
         self.initiative[initiative].append(combatant)
 
+    def print_combatants(self):
+        for c in self.combatants:
+            print(str(c))
+
     def print_initiative(self):
-        for i in initiative:
-            for c in i:
-                print(str(c))
+        for i in self.initiative:
+            print(str(i))
 
     def print_initiative_combatants(self) -> None:
         print(str(self.initiative_combatants))
 
 def main():
     combat_initiative = Initiative()
-    combat_initiative.add_combatant(1, 'Steve', 1)
-    combat_initiative.add_combatant(20, 'Alex', 2)
+    steve = combatant.Combatant('Steve', None)
+    steve.change_stats(steve.Stats.DEX, 20)
+    alex = combatant.Combatant('Alex', None)
+    alex.change_stats(alex.Stats.DEX, 10)
+    combat_initiative.add_combatant(steve, 20, 1)
+    combat_initiative.add_combatant(alex, 20, 2)
+    combat_initiative.print_combatants()
     combat_initiative.print_initiative()
+    print('This is the end of main()')
     exit(0)
 
 if __name__=='__main__':
