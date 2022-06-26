5ea9b134433517e5475c3dc1e15274515e414cc2
Daniel Abrego <danielabrego@utexas.edu>
2022-06-26 00:44:48 -0500

    added simple testing to check initiative class is importable and can be instantiated

--- simple_main.py
+++ simple_main.py
@@ -1,7 +1,9 @@
 from dnd import combatant
+from dnd import initiative
 
 def main():
-    first_combatant = combatant.Combatant()
+    first_combatant = combatant.Combatant("Test", None)
+    combat_initiative = initiative.Initiative()
     exit(0)
 
 if __name__=='__main__':
